8186f4e47003640b7c2b770345a00cab8283c73b
Chinmaya Mohapatra <145309196+Blakkyx@users.noreply.github.com>
2024-09-12 19:25:05 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -1,103 +1,79 @@
- <!-- Header-->
- <header class="bg-dark py-5" id="main-header">
-    <div class="container h-100 d-flex align-items-end justify-content-center w-100">
-        <div class="text-center text-white w-100">
-            <h1 class="display-4 fw-bolder"><?php echo $_settings->info('name') ?></h1>
-            <p class="lead fw-normal text-white-50 mb-0">We will take care of your vehicle</p>
-            <div class="col-auto mt-2">
-                <button class="btn btn-primary btn-lg rounded-0" id="send_request" type="button">Send Service Request</button>
-            </div>
-        </div>
-    </div>
-</header>
-<!-- Section-->
-<section class="py-5">
-    <div class="container px-4 px-lg-5 mt-5">
-        <div class="row">
-            <div class="col-md-4">
-                <h3 class="text-center">We Do Service For:</h3>
-                <hr class="bg-primary opacity-100">
-                <ul class="list-group">
-                    <?php 
-                    $category = $conn->query("SELECT * FROM `categories` where status = 1 order by `category` asc");
-                    while($row=$category->fetch_assoc()):
-                    ?>
-                    <li class="list-group-item"><b><?php echo $row['category'] ?></b></li>
-                    <?php endwhile; ?>
-                </ul>
-            </div>
-            <div class="col-md-8">
-                <h3 class="text-center">Our Services</h3>
-                <hr class="bg-primary opacity-100">
-                <div class="form-group">
-                <div class="input-group mb-3">
-                    <input type="search" id="search" class="form-control" placeholder="Search Service Here" aria-label="Search Service Here" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                        <span class="input-group-text bg-primary" id="basic-addon2"><i class="fa fa-search"></i></span>
-                    </div>
-                </div>
-                </div>
-                <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-xl-2" id="service_list">
-                    <?php 
-                    $services = $conn->query("SELECT * FROM `service_list` where status = 1 order by `service`");
-                    while($row= $services->fetch_assoc()):
-                        $row['description'] = strip_tags(html_entity_decode(stripslashes($row['description'])));
-                    ?>
-                    <a class="col item text-decoration-none text-dark view_service" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>">
-                        <div class="callout callout-primary border-primary rounded-0">
-                            <dl>
-                                <dt><?php echo $row['service'] ?></dt>
-                                <dd class="truncate-3 text-muted lh-1"><small><?php echo $row['description'] ?></small></dd>
-                            </dl>
-                        </div>
-                    </a>
-                    <?php endwhile; ?>
-                </div>
-                <div id="noResult" style="display:none" class="text-center"><b>No Result</b></div>
-            </div>
-        </div>
-    </div>
-</section>
-<script>
-    $(function(){
-        $('#search').on('input',function(){
-            var _search = $(this).val().toLowerCase().trim()
-            $('#service_list .item').each(function(){
-                var _text = $(this).text().toLowerCase().trim()
-                    _text = _text.replace(/\s+/g,' ')
-                    console.log(_text)
-                if((_text).includes(_search) == true){
-                    $(this).toggle(true)
-                }else{
-                    $(this).toggle(false)
-                }
-            })
-            if( $('#service_list .item:visible').length > 0){
-                $('#noResult').hide('slow')
-            }else{
-                $('#noResult').show('slow')
-            }
-        })
-        $('#service_list .item').hover(function(){
-            $(this).find('.callout').addClass('shadow')
-        })
-        $('#service_list .view_service').click(function(){
-            uni_modal("Service Details","view_service.php?id="+$(this).attr('data-id'),'mid-large')
-        })
-        $('#send_request').click(function(){
-            uni_modal("Fill the Service Request Form","send_request.php",'large')
-        })
+<h1 class="text-light">Welcome to <?php echo $_settings->info('name') ?></h1>
+<hr class="border-light">
+<div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-light elevation-1"><i class="fas fa-th-list"></i></span>
 
-    })
-    $(document).scroll(function() { 
-        $('#topNavBar').removeClass('bg-transparent navbar-dark bg-primary')
-        if($(window).scrollTop() === 0) {
-           $('#topNavBar').addClass('navbar-dark bg-transparent')
-        }else{
-           $('#topNavBar').addClass('navbar-dark bg-primary')
-        }
-    });
-    $(function(){
-        $(document).trigger('scroll')
-    })
-</script>
+              <div class="info-box-content">
+                <span class="info-box-text">Total Category</span>
+                <span class="info-box-number">
+                  <?php 
+                    $inv = $conn->query("SELECT count(id) as total FROM categories ")->fetch_assoc()['total'];
+                    echo number_format($inv);
+                  ?>
+                  <?php ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users-cog"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Mechanics</span>
+                <span class="info-box-number">
+                  <?php 
+                    $mechanics = $conn->query("SELECT sum(id) as total FROM `mechanics_list` where status = '1' ")->fetch_assoc()['total'];
+                    echo number_format($mechanics);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+
+          <!-- fix for small devices only -->
+          <div class="clearfix hidden-md-up"></div>
+
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-th-list"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Services</span>
+                <span class="info-box-number">
+                <?php 
+                    $services = $conn->query("SELECT sum(id) as total FROM `service_list` where status = 1 ")->fetch_assoc()['total'];
+                    echo number_format($services);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-file-invoice"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Finished Requests</span>
+                <span class="info-box-number">
+                <?php 
+                    $services = $conn->query("SELECT sum(id) as total FROM `service_requests` where status = 3 ")->fetch_assoc()['total'];
+                    echo number_format($services);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+        </div>
